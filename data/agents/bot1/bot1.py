c78137728b9b5216ce77622580ed4071cbca6985
MuNeNiCK <mune0127@munenick.me>
2022-12-24 13:20:55 +0000

    Co-authored-by: shiroh4ck <shiroh4ck@users.noreply.github.com> Co-authored-by: ejirishoma <ejirishoma@users.noreply.github.com>

--- data/agents/bot1/bot1.py
+++ data/agents/bot1/bot1.py
@@ -2,6 +2,8 @@ import os
 from requests import sessions
 from rocketchat_API.rocketchat import RocketChat
 
+from . import bot1_ai
+
 def start_bot1():
     username = "BOT1"
     password = os.environ.get('ROCKET_PASSWORD')
@@ -12,3 +14,5 @@ def start_bot1():
 
         rocket.chat_post_message('BOT1です', channel='GENERAL')
 
+    response = bot1_ai.handle_response("世界で一番人口が多い国は？")
+    rocket.chat_post_message(response, channel='GENERAL')
